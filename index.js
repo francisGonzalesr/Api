1e8b46f8820004b46afcef319fe2a66042bdb8bf
Ruby <rubyfg70@gmail.com>
2021-07-27 23:37:15 -0600

    agregando index
    prueba

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 const userRouter = require('./routes/user.router');
 app.set('puerto', process.env.PORT || 4000);
-
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
@@ -12,3 +12,4 @@ app.use(cors());
 app.use('/users', userRouter);
 
 app.listen(app.get('puerto'), () => console.log('Server listo'));
+
